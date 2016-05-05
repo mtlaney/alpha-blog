41c7bf965186f7a6fe797c0d56f8ffe38ca26700
mtlaney <mtlaney@gmail.com>
2016-05-05 20:34:39 +0000

    add users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def index
+    @users = User.all
+  end
 
   def create
       @user = User.new(user_params)
@@ -31,6 +35,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
 
   private
   def user_params
